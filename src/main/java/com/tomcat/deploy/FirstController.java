55005ca4f2383c9627df86610c59cb8a2ce672f3
vinodh1988 <k.c.vinodh@hotmail.com>
2021-09-23 06:26:37 +0000

    jenkins-commit

--- src/main/java/com/tomcat/deploy/FirstController.java
+++ src/main/java/com/tomcat/deploy/FirstController.java
@@ -10,7 +10,7 @@ public class FirstController {
 	String n[]= {"James","Sharan","Hari"};
   @GetMapping("/greet")
   public String greet() {
-	  return "Updated Spring boot is running";
+	  return "Updated Spring boot is running with pipeline script";
   }
   
   @GetMapping("/greet2")
